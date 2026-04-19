4d01278a416fdb61624f4e3a79e1250830071b27
onlyaayatish <onlyaayatish@gmail.com>
2026-04-19 22:28:15 +0545

    Updated Final student.py

--- student.py
+++ student.py
@@ -1,9 +1,6 @@
 from file_handler import read_users, get_grades, get_eca, write_users
 
-
-# ===============================
 # Helper Functions
-# ===============================
 
 def get_user_data(user_id):
     for user in read_users():
@@ -11,10 +8,7 @@ def get_user_data(user_id):
             return user
     return None
 
-
-# ===============================
 # Main Student Features
-# ===============================
 
 def view_profile(user_id):
     user = get_user_data(user_id)
@@ -63,9 +57,7 @@ def update_profile(user_id):
     print("Profile updated successfully!")
 
 
-# ===============================
 # Student Menu (unchanged)
-# ===============================
 
 def student_menu(user_id):
     while True:
